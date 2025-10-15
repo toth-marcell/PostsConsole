91fb9fb46b146e41eb7c0046c09f208432f39f07
toth-marcell <toth.marcell.21i@egressy.info>
2025-10-15 09:05:40 +0200

    cleanup

--- MenuOption.cs
+++ MenuOption.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PostsConsole
+﻿namespace PostsConsole
 {
     internal struct MenuOption(string name, Func<Task> execute)
     {
--- ServerConnection.cs
+++ ServerConnection.cs
@@ -1,7 +1,5 @@
-﻿using System.Net;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace PostsConsole
 {
